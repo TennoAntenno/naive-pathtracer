f05b7da34c9d9e09837940eaf13b847732bffa1f
TennoAntenno <jedidja873@gmail.com>
2025-10-03 19:00:52 +0300

    Create dotnet project, setup raylib

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Raylib_cs;
+
+namespace naive_pathtracer;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Raylib.InitWindow(800, 450, "naive pathtracer");
+        Raylib.SetTargetFPS(60);
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.Black);
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
